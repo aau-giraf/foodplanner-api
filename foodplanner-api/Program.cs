995566684bd1ae58f0cdeac2162a67d7434b4a24
DogTagWarrior <jako6724@gmail.com>
2024-09-26 15:38:49 +0200

    setup dapper an intialize a connection to database

--- foodplanner-api/Program.cs
+++ foodplanner-api/Program.cs
@@ -1,9 +1,24 @@
+using Dapper;
+using foodplannerApi.Controller;
+using foodplannerApi.Models;
+using foodplannerApi.Services;
+using Npgsql;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddSingleton(serviceProvider => {
+    var configuration = serviceProvider.GetRequiredService<IConfiguration>();
+
+    var connectionString = configuration.GetConnectionString("DefaultConnection") ?? 
+        throw new ApplicationException("the connection string is null");
+
+    return new PostgreConnectionFactory(connectionString);
+});
 
 var app = builder.Build();
 
@@ -16,27 +31,7 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-
-
-.WithName("GetWeatherForecast")
-.WithOpenApi();
+app.MapUserControllers();
 
 app.MapGet("/test", () => "Testing sdhashaSCVHK!")
 .WithName("GetTest")
@@ -46,8 +41,3 @@ app.MapGet("/test", () => "Testing sdhashaSCVHK!")
 app.Urls.Add("http://0.0.0.0:80");
 
 app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
