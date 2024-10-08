7f60eeeb71ae3a757fdca380997e2d24e9c90289
alexaaskov <EQ15JQ@student.aau.dk>
2024-10-08 10:01:55 +0200

    Fixed inaccesability of the Meal class

--- FoodplannerModels/Lunchbox/Meal.cs
+++ FoodplannerModels/Lunchbox/Meal.cs
@@ -6,11 +6,12 @@ namespace FoodplannerModels.Lunchbox;
 * Contains an image of the element and an alternate text in case the image cannot be loaded or is not shown for other reasons.
 */
 public class Meal {
-    private string Name {get; set;}
-    private string? Description {get; set;} // A description or comment on how the meal should be eaten/prepared.
-    private LinkedList<Ingredient> Ingredients {get; set;}
+    public int Id{get; set;}
+    public string Name {get; set;}
+    public string? Description {get; set;} // A description or comment on how the meal should be eaten/prepared.
+    public LinkedList<Ingredient> Ingredients {get; set;}
     //private Image parameter
-    private string AltText {get; set;} // Alternate text in case the image is not shown
+    public string AltText {get; set;} // Alternate text in case the image is not shown
 
     /**
     * @Name the name of the meal
@@ -18,9 +19,9 @@ public class Meal {
     * @AltText text shown if the image is not loaded
     * @returns an instance of the Meal class
     */
-    public Meal(string Name, /*Image Image,*/ string AltText) {
-        this.Name = Name;
-        // this.Image = Image;
-        this.AltText = AltText;
-    }
+    // public Meal(string Name, /*Image Image,*/ string AltText) {
+    //     this.Name = Name;
+    //     // this.Image = Image;
+    //     this.AltText = AltText;
+    // }
 }
