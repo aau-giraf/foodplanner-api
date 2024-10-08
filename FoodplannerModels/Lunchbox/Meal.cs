493e76c13374fb480f5e4ef48cea7082a3e486ee
alexaaskov <EQ15JQ@student.aau.dk>
2024-10-08 09:45:22 +0200

    Fixed class duplicates

--- FoodplannerModels/Lunchbox/Meal.cs
+++ FoodplannerModels/Lunchbox/Meal.cs
@@ -1,11 +1,26 @@
 namespace FoodplannerModels.Lunchbox;
 
 /**
-* Temperary class for the meal.
+* Class for individual meals in the foodplanner.
+* Contains the name of the Meal and description for commenting/describing the Meal
+* Contains an image of the element and an alternate text in case the image cannot be loaded or is not shown for other reasons.
 */
-public class Meal
-{
-    public int Id { get; set; }
-    public required string Meal_name { get; set; }
-    public required LinkedList<Ingredient> Meal_ingredients { get; set; }
+public class Meal {
+    private string Name {get; set;}
+    private string? Description {get; set;} // A description or comment on how the meal should be eaten/prepared.
+    private LinkedList<Ingredient> Ingredients {get; set;}
+    //private Image parameter
+    private string AltText {get; set;} // Alternate text in case the image is not shown
+
+    /**
+    * @Name the name of the meal
+    * @Image an image file related to the meal
+    * @AltText text shown if the image is not loaded
+    * @returns an instance of the Meal class
+    */
+    public Meal(string Name, /*Image Image,*/ string AltText) {
+        this.Name = Name;
+        // this.Image = Image;
+        this.AltText = AltText;
+    }
 }
