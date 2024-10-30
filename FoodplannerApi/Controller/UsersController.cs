0c1ee2330c275c871615ab1c52dfddaac4866aee
alexrefshauge <arefsh22@student.aau.dk>
2024-10-30 13:38:01 +0100

    annotate user controller

--- FoodplannerApi/Controller/UsersController.cs
+++ FoodplannerApi/Controller/UsersController.cs
@@ -4,6 +4,7 @@ using FoodplannerServices;
 using FoodplannerServices.Account;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
 
 namespace FoodplannerApi.Controller;
 
@@ -15,7 +16,9 @@ public class UsersController : BaseController {
         _userService = userService;
         _authService = authService;
     }
+    
     [HttpGet]
+    [ProducesResponseType(typeof(string), StatusCodes.Status200OK)]
     public async Task<IActionResult> GetBearerTest()
     {
         //Generates a token for development purposes, Status must be Active.
@@ -48,6 +51,7 @@ public class UsersController : BaseController {
 
 
     [HttpPost]
+    [ProducesResponseType(typeof(string), StatusCodes.Status200OK)]
     public async Task<IActionResult> Create([FromBody] UserCreateDTO userCreate){
         if (!ModelState.IsValid){
             return BadRequest(ModelState);
@@ -64,6 +68,10 @@ public class UsersController : BaseController {
     }
     
     [HttpPost]
+    [ProducesResponseType(typeof(string), StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(ModelStateDictionary), StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+
     public async Task<IActionResult> Login([FromBody] Login user){
         if (!ModelState.IsValid){
             return BadRequest(ModelState);
