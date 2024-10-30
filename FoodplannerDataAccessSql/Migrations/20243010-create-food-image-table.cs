bc7fa3e9656acc98a6960c0f129455fdbd359801
Androkless <NO01VU@student.aau.dk>
2024-10-30 15:23:24 +0100

    Skub skub skub til hubben

---
+++ FoodplannerDataAccessSql/Migrations/20243010-create-food-image-table.cs
@@ -0,0 +1,23 @@
+﻿using FluentMigrator;
+
+namespace FoodplannerDataAccessSql.Migrations;
+
+
+[Migration(20243010)]
+public class create_food_image_table : Migration {
+    
+    public override void Up() {
+        Create.Table("food_image")
+            .WithColumn("id").AsInt32().PrimaryKey().Identity()
+            .WithColumn("image_id").AsString().NotNullable()
+            .WithColumn("user_id").AsInt32().NotNullable()
+            .WithColumn("image_name").AsString().NotNullable()
+            .WithColumn("image_file_type").AsString().NotNullable()
+            .WithColumn("size").AsInt64().NotNullable();
+    }
+
+    public override void Down() {
+        Delete.Table("food_image");
+    }
+    
+}
