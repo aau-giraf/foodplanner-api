f0481dfa7e134266a8f7e66bfe3a3c5eb92e6adc
alexrefshauge <arefsh22@student.aau.dk>
2024-10-30 13:42:44 +0100

    annotate admin controller

--- FoodplannerApi/Controller/AdminController.cs
+++ FoodplannerApi/Controller/AdminController.cs
@@ -15,6 +15,8 @@ public class AdminController : BaseController
     }
    
     [HttpDelete("{id}")]
+    [ProducesResponseType(StatusCodes.Status204NoContent)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
     public async Task<IActionResult> Delete(int id){
         var result = await _userService.DeleteUserAsync(id);
         if (result > 0){
@@ -23,11 +25,15 @@ public class AdminController : BaseController
         return NotFound();
     }
     [HttpGet]
+    [ProducesResponseType(typeof(IEnumerable<UserDTO>), StatusCodes.Status200OK)]
     public async Task<IActionResult> GetAll(){
         var users = await _userService.GetAllUsersAsync();
         return Ok(users);
     }
+    
     [HttpGet("{id}")]
+    [ProducesResponseType(typeof(IEnumerable<UserDTO>), StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
     public async Task<IActionResult> Get(int id){
         var users = await _userService.GetUserByIdAsync(id);
         if (users == null){
@@ -35,7 +41,11 @@ public class AdminController : BaseController
         }
         return Ok(users);
     }
+    
     [HttpPut("{id}")]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(StatusCodes.Status204NoContent)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
     public async Task<IActionResult> Update(int id, [FromBody] User user){
         if (id != user.Id){
             return BadRequest();
