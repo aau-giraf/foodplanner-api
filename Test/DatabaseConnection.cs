1e69f4c67f3abd7d78c16ce6854c319e3aac577b
alexaaskov <EQ15JQ@student.aau.dk>
2024-10-31 09:40:44 +0100

    Removed security breach

--- Test/DatabaseConnection.cs
+++ Test/DatabaseConnection.cs
@@ -5,11 +5,12 @@ namespace testing;
 
 static class DatabaseConnection
 {
-    private static string host = "10.92.0.69";
-    private static string port = "5432";
-    private static string database = "giraf_foodplanner_db_sw5_10";
-    private static string username = "postgres";
-    private static string password = "yjF45K#6rmRd@2RJbbLA";
+    //Until fixed, you need to fill it in your self.
+    private static string host = "";
+    private static string port = "";
+    private static string database = "";
+    private static string username = "";
+    private static string password = "";
 
     public static PostgreSQLConnectionFactory GetConnection()
     {
