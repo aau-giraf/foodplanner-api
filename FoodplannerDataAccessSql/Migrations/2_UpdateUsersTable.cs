349dcaa5de106f3f7be45848a1d7fe6e863a2a53
Androkless <NO01VU@student.aau.dk>
2024-11-14 14:19:43 +0100

    Changed Users table

---
+++ FoodplannerDataAccessSql/Migrations/2_UpdateUsersTable.cs
@@ -0,0 +1,31 @@
+﻿using FluentMigrator;
+using Microsoft.Extensions.Primitives;
+
+namespace FoodplannerDataAccessSql.Migrations;
+
+
+[Migration(2)]
+public class updateUsersTable: Migration 
+{
+    public override void Up()
+    {
+        Alter.Column("role_approved").OnTable("users").AsBoolean().NotNullable();
+        Alter.Column("id").OnTable("users").AsInt32().PrimaryKey().NotNullable().Identity();
+        Alter.Column("pincode").OnTable("users").AsString(100).Nullable();
+        Alter.Table("users")
+            .AddColumn("archived").AsBoolean().NotNullable().WithDefaultValue(false);
+    }
+
+    public override void Down()
+    {
+        Alter.Column("role_approved").OnTable("users").AsString().NotNullable();
+        
+        Delete.PrimaryKey("PK_users").FromTable("users");
+        Delete.Column("id").FromTable("users");
+        Alter.Table("users")
+            .AddColumn("id").AsInt32().PrimaryKey().NotNullable();
+        
+        Alter.Column("pincode").OnTable("users").AsString(100).NotNullable();
+        Delete.Column("archived").FromTable("users");
+    }
+}
