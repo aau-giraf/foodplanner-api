51f655d3e80580573c96ced545ccadda4a0eda7b
alexaaskov <EQ15JQ@student.aau.dk>
2024-12-08 14:52:09 +0100

    Added Comments

--- Test/DatabaseFixture.cs
+++ Test/DatabaseFixture.cs
@@ -1,13 +1,17 @@
 using FoodplannerDataAccessSql;
 using FoodplannerDataAccessSql.Migrations;
-using Infisical.Sdk;
 using Microsoft.Extensions.Configuration;
 using Dapper;
 using FluentMigrator.Runner;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Test.Repository;
-
+/**
+/* This class contains the connection to the PostgreSQL Docker Container.
+/* It contains methods for adding test elements to the database as well as a method
+/* for cleaning the database. It is currently only setup to clean all elements that
+/* are users or has a foreign key to users.
+*/
 public class DatabaseFixture : IDisposable
 {
     private readonly PostgreSQLConnectionFactory factory;
@@ -17,6 +21,7 @@ public class DatabaseFixture : IDisposable
     private string username;
     private string password;
 
+    // Constructor that creates the PostgreSQLConnectionFactory from a seetings.json file.
     public DatabaseFixture()
     {
         // Load settings from settings.json
@@ -34,9 +39,11 @@ public class DatabaseFixture : IDisposable
 
         factory = new PostgreSQLConnectionFactory(host, port, database, username, password);
 
+        // Adds all the tables to the connected database.
         ApplyMigrations();
     }
 
+    // Method for fetching values stored in the configuration and validating them.
     private string GetRequiredSetting(IConfiguration configuration, string key)
     {
         var value = configuration[key];
@@ -47,58 +54,7 @@ public class DatabaseFixture : IDisposable
         return value;
     }
 
-    public PostgreSQLConnectionFactory GetFactory() => factory;
-
-    public async Task<int> CreateTestUser()
-    {
-        var sql = "INSERT INTO users (first_name, last_name, email, password, role, role_approved) VALUES ('test', 'test', 'test@example.com', '1234', 'Parent', 'true') RETURNING id";
-        using (var connection = factory.Create())
-        {
-            connection.Open();
-            return await connection.QuerySingleAsync<int>(sql);
-        }
-    }
-
-    public async Task<int> CreateTestImage(int userId)
-    {
-        var sql = $"INSERT INTO food_image (image_id, user_id, image_name, image_file_type, size) VALUES ('1', '{userId}', 'test', 'none', '0') RETURNING id";
-        using (var connection = factory.Create())
-        {
-            connection.Open();
-            return await connection.QuerySingleAsync<int>(sql);
-        }
-    }
-
-    public async Task<int> CreateTestMeal(int userId, int imageId)
-    {
-        var sql = $"INSERT INTO meals (name, user_id, food_image_id, date) VALUES ('test', '{userId}', '{imageId}', 'test') RETURNING id";
-        using (var connection = factory.Create())
-        {
-            connection.Open();
-            return await connection.QuerySingleAsync<int>(sql);
-        }
-    }
-
-    public async Task<int> CreateTestIngredient(int userId, int imageId)
-    {
-        var sql = $"INSERT INTO ingredients (name, user_id, food_image_id) VALUES ('test', '{userId}', '{imageId}') RETURNING id";
-        using (var connection = factory.Create())
-        {
-            connection.Open();
-            return await connection.QuerySingleAsync<int>(sql);
-        }
-    }
-
-    public async void ResetDatabase()
-    {
-        var sql = "TRUNCATE TABLE users CASCADE";
-        using (var connection = factory.Create())
-        {
-            connection.Open();
-            await connection.ExecuteAsync(sql);
-        }
-    }
-
+    // Method for running the migration.
     private void ApplyMigrations()
     {
         using var scope = CreateMigrationScope();
@@ -111,6 +67,7 @@ public class DatabaseFixture : IDisposable
         }
     }
 
+    // Method for defining where the migration should take place.
     private IServiceScope CreateMigrationScope()
     {
         var serviceCollection = new ServiceCollection();
@@ -130,6 +87,67 @@ public class DatabaseFixture : IDisposable
         return serviceCollection.BuildServiceProvider().CreateScope();
     }
 
+    // Method for getting the connection factory from the initialized object.
+    public PostgreSQLConnectionFactory GetFactory() => factory;
+
+    // Method for directly creating a user on the database.
+    public async Task<int> CreateTestUser()
+    {
+        var sql = "INSERT INTO users (first_name, last_name, email, password, role, role_approved) VALUES ('test', 'test', 'test@example.com', '1234', 'Parent', 'true') RETURNING id";
+        using (var connection = factory.Create())
+        {
+            connection.Open();
+            return await connection.QuerySingleAsync<int>(sql);
+        }
+    }
+
+    // Method for directly creating an image on the database.
+    public async Task<int> CreateTestImage(int userId)
+    {
+        var sql = $"INSERT INTO food_image (image_id, user_id, image_name, image_file_type, size) VALUES ('1', '{userId}', 'test', 'none', '0') RETURNING id";
+        using (var connection = factory.Create())
+        {
+            connection.Open();
+            return await connection.QuerySingleAsync<int>(sql);
+        }
+    }
+
+    // Method for directly creating a meal on the database.
+    public async Task<int> CreateTestMeal(int userId, int imageId)
+    {
+        var sql = $"INSERT INTO meals (name, user_id, food_image_id, date) VALUES ('test', '{userId}', '{imageId}', 'test') RETURNING id";
+        using (var connection = factory.Create())
+        {
+            connection.Open();
+            return await connection.QuerySingleAsync<int>(sql);
+        }
+    }
+
+    // Method for directly creating an ingredient on the database.
+    public async Task<int> CreateTestIngredient(int userId, int imageId)
+    {
+        var sql = $"INSERT INTO ingredients (name, user_id, food_image_id) VALUES ('test', '{userId}', '{imageId}') RETURNING id";
+        using (var connection = factory.Create())
+        {
+            connection.Open();
+            return await connection.QuerySingleAsync<int>(sql);
+        }
+    }
+
+    // Method for emptying the users table as well as all other tables with
+    // foreign keys to the users table.
+    public async void ResetDatabase()
+    {
+        var sql = "TRUNCATE TABLE users CASCADE";
+        using (var connection = factory.Create())
+        {
+            connection.Open();
+            await connection.ExecuteAsync(sql);
+        }
+    }
+
+    // DatabaseFixture uses the Disposable interface, which means that after all tests
+    // that uttalizes this class are run, the Dispose method is called.
     public void Dispose()
     {
         ResetDatabase();
