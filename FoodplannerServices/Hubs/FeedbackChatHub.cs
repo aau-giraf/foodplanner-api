666bd2072175892166931dd4114958be8103f1a8
lindejacob <lindejacob0612@gmail.com>
2025-09-22 07:06:00 +0200

    Implemented Interface for stongly typed hub

--- FoodplannerServices/Hubs/FeedbackChatHub.cs
+++ FoodplannerServices/Hubs/FeedbackChatHub.cs
@@ -1,17 +1,22 @@
 using Microsoft.AspNetCore.SignalR;
 namespace FoodplannerServices.Hubs;
 
-public class FeedbackChatHub : Hub
+public class FeedbackChatHub : Hub<IFeedbackChatClient>
 {
     public override async Task OnConnectedAsync()
     {
-        await Clients.All.SendAsync("ReceiveMessage", "Feedback connected");
+        await Clients.All.ReceiveMessage("System", "Feedback connected");
     }
 
-
-    public async Task NewMessage(long userId, string message)
+    public async Task SendMessage(string user, string message)
     {
-        await Clients.Client(Context.ConnectionId).SendAsync("RecivedMessage", userId, message);
+        await Clients.User(user).ReceiveMessage(user, message);
+        // Add logic to save message to database
     }
 
-};
+    public async Task<bool> ArchiveMessageAsync(int messageId)
+    {
+        // Implement your logic to archive the message here
+        return await Task.FromResult(true);
+    }
+}
---
+++ FoodplannerServices/Hubs/IFeedbackChatClient.cs
@@ -0,0 +1,6 @@
+namespace FoodplannerServices.Hubs;
+
+public interface IFeedbackChatClient
+{
+    Task ReceiveMessage(string user, string message);
+}
