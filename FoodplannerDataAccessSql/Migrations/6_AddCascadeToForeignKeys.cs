6da35cadf26700de34d519542849c148253c85be
lindejacob <lindejacob0612@gmail.com>
2025-10-22 11:18:38 +0200

    Used FluentMigrator to do migrations instead of normal SQL. And remvoed unused function

--- FoodplannerDataAccessSql/Migrations/6_AddCascadeToForeignKeys.cs
+++ FoodplannerDataAccessSql/Migrations/6_AddCascadeToForeignKeys.cs
@@ -1,25 +1,27 @@
 using FluentMigrator;
-
-namespace FoodplannerDataAccessSql.Migrations;
-
-[Migration(6)]
-public class AddCascadeToForeignKeys : Migration
-{
-    public override void Up()
+using System.Data;
+    
+    namespace FoodplannerDataAccessSql.Migrations;
+    
+    [Migration(6)]
+    public class AddCascadeToForeignKeys : Migration
     {
-        Execute.Sql(@"ALTER TABLE IF EXISTS packed_ingredients DROP CONSTRAINT IF EXISTS fk_packed_ingredients_ingredient_id;"
-                    + @"ALTER TABLE packed_ingredients ADD CONSTRAINT fk_packed_ingredients_ingredient_id FOREIGN KEY (ingredient_id) REFERENCES ingredients(id) ON DELETE CASCADE;");
-        
-        Execute.Sql(@"ALTER TABLE IF EXISTS packed_ingredients DROP CONSTRAINT IF EXISTS fk_packed_ingredients_meal_id;"
-                    + @"ALTER TABLE packed_ingredients ADD CONSTRAINT fk_packed_ingredients_meal_id FOREIGN KEY (meal_id) REFERENCES meals(id) ON DELETE CASCADE;");
+        public override void Up()
+        {
+            Delete.ForeignKey("fk_packed_ingredients_ingredient_id").OnTable("packed_ingredients");
+    
+            Create.ForeignKey("fk_packed_ingredients_ingredient_id")
+                .FromTable("packed_ingredients").ForeignColumn("ingredient_id")
+                .ToTable("ingredients").PrimaryColumn("id")
+                .OnDelete(Rule.Cascade);
+        }
+    
+        public override void Down()
+        {
+            Delete.ForeignKey("fk_packed_ingredients_ingredient_id").OnTable("packed_ingredients");
+    
+            Create.ForeignKey("fk_packed_ingredients_ingredient_id")
+                .FromTable("packed_ingredients").ForeignColumn("ingredient_id")
+                .ToTable("ingredients").PrimaryColumn("id");
+        }
     }
-
-    public override void Down()
-    {
-        Execute.Sql(@"ALTER TABLE IF EXISTS packed_ingredients DROP CONSTRAINT IF EXISTS fk_packed_ingredients_ingredient_id;"
-                    + @"ALTER TABLE packed_ingredients ADD CONSTRAINT fk_packed_ingredients_ingredient_id FOREIGN KEY (ingredient_id) REFERENCES ingredients(id)");
-        
-        Execute.Sql(@"ALTER TABLE IF EXISTS packed_ingredients DROP CONSTRAINT IF EXISTS fk_packed_ingredients_meal_id;"
-                    + @"ALTER TABLE packed_ingredients ADD CONSTRAINT fk_packed_ingredients_meal_id FOREIGN KEY (meal_id) REFERENCES meals(id);");
-    }
-}
