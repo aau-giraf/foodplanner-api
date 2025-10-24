bf858d00cdd59bc77cb208a3dab66b00657a6717
lindejacob <lindejacob0612@gmail.com>
2025-10-24 14:02:20 +0200

    Added migration

---
+++ FoodplannerDataAccessSql/Migrations/8_UserIdOnChildrenTable.cs
@@ -0,0 +1,29 @@
+using FluentMigrator;
+using System.Data;
+
+namespace FoodplannerDataAccessSql.Migrations;
+
+[Migration(8)]
+public class UserIdOnChildrenTable : Migration
+{
+    public override void Up()
+    {
+        Create.ForeignKey("fk_children_child_id")
+            .FromTable("children").ForeignColumn("child_id")
+            .ToTable("users").PrimaryColumn("id")
+            .OnDeleteOrUpdate(Rule.Cascade);
+        
+        Execute.Sql(@"
+            UPDATE children c
+            SET child_id = u.id
+            FROM users u
+            WHERE LOWER(c.first_name) = LOWER(u.first_name)
+              AND LOWER(c.last_name) = LOWER(u.last_name);
+        ");
+    }
+
+    public override void Down()
+    {
+        Delete.ForeignKey("fk_children_chi_id").OnTable("children");
+    }
+}
