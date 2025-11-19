15d146975b348524c2019841d32bf4af4fe61bfb
lindejacob <lindejacob0612@gmail.com>
2025-11-19 14:17:04 +0100

    Removed ability to delete and create child, through endpoint

--- FoodplannerApi/Controller/ChildrensController.cs
+++ FoodplannerApi/Controller/ChildrensController.cs
@@ -35,55 +35,6 @@ public class ChildrensController : BaseController
         return Ok(children);
     }
     
-    [HttpPost]
-    public async Task<IActionResult> Create([FromHeader(Name = "Authorization")] string token, [FromBody] ChildrenCreateDTO childrenCreate)
-    {
-        if (!ModelState.IsValid)
-        {
-            return BadRequest(ModelState);
-        }
-        try
-        {
-            var idString = _authService.RetrieveIdFromJwtToken(token); // Use the method to get the parentId from the token    
-            if (!int.TryParse(idString, out int parentId))
-            {
-                return BadRequest(new ErrorResponse { Message = new[] { "Id er ikke et tal" } });
-            }
-
-            var childToCreate = new ChildrenCreateParentDTO
-            {
-                FirstName = childrenCreate.FirstName,
-                LastName = childrenCreate.LastName,
-                ParentIds = new List<int> { parentId },
-                classId = childrenCreate.ClassId
-            };
-
-            var id = await _childrenService.CreateChildrenAsync(childToCreate);
-            if (id > 0)
-            {
-                return Created(string.Empty, id);
-            }
-            return BadRequest();
-        }
-        catch (InvalidOperationException e)
-        {
-            return BadRequest(e.Message);
-        }
-    }
-
-    [Authorize(Policy = "AdminPolicy")]
-    [HttpDelete("{id}")]
-    public async Task<IActionResult> Delete(int id)
-    {
-        var result = await _childrenService.DeleteChildrenAsync(id);
-        if (result > 0)
-        {
-            return NoContent();
-        }
-        return NotFound();
-    }
-
-
     [HttpGet]
     public async Task<IActionResult> GetChildrenByParentId([FromHeader(Name = "Authorization")] string token)
     {
