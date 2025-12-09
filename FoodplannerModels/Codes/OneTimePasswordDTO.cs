1049dfc3b271957391ea8c7291dd5907a243d46b
lindejacob <lindejacob0612@gmail.com>
2025-12-09 09:58:34 +0100

    Added required in the DTO

--- FoodplannerModels/Codes/OneTimePasswordDTO.cs
+++ FoodplannerModels/Codes/OneTimePasswordDTO.cs
@@ -2,8 +2,11 @@ namespace FoodplannerModels.Codes;
 using System.ComponentModel.DataAnnotations;
 public class OneTimePasswordDTO
 {
+    [Required]
     required public int CodeId {  get; set; }
+    [Required]
     required public int GeneratedBy {  get; set; }
+    [Required]
     required public bool Used {  get; set; } 
     public int? UsedByUser { get; set; }
     public int? ChildUser { get; set; }
