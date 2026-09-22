c2852493cb097cf583764a9faaa1591a3d080cc2
Alenje2004 <WS10ZL@student.aau.dk>
2026-09-22 13:16:07 +0200

    Added documentation for ImagesController, updated related tests, and homogonized controller creation.

--- FoodplannerApi/Controller/ClassroomsController.cs
+++ FoodplannerApi/Controller/ClassroomsController.cs
@@ -6,21 +6,15 @@ using Microsoft.AspNetCore.Mvc;
 namespace FoodplannerApi.Controller;
 
 
-public class ClassroomsController : BaseController
+public class ClassroomsController(IClassroomService classroomService) : BaseController
 {
-
-    // Setup of controller with injections to the service layer for classrooms.
-    private readonly IClassroomService _classroomService;
-    public ClassroomsController(IClassroomService classroomService){
-        _classroomService = classroomService;
-    }
 
     // URL: api/Classrooms/GetAll
     // Retrieves all classrooms. Returns 200 OK with the list of classrooms.
     [HttpGet]
     [ProducesResponseType(typeof(IEnumerable<ClassroomDTO>), StatusCodes.Status200OK)]
     public async Task<IActionResult> GetAll(){
-        var classroom = await _classroomService.GetAllClassroomAsync();
+        var classroom = await classroomService.GetAllClassroomAsync();
         return Ok(classroom);
     }
 
@@ -31,7 +25,7 @@ public class ClassroomsController : BaseController
     [ProducesResponseType(typeof(int), StatusCodes.Status201Created)]
     [ProducesResponseType(StatusCodes.Status400BadRequest)]
     public async Task<ActionResult> Create([FromBody] CreateClassroomDTO createClassroomDTO){
-        var result = await _classroomService.InsertClassroomAsync(createClassroomDTO);
+        var result = await classroomService.InsertClassroomAsync(createClassroomDTO);
         if (result > 0){
              return Created(string.Empty, result);
         }
@@ -45,7 +39,7 @@ public class ClassroomsController : BaseController
     [ProducesResponseType(typeof(int), StatusCodes.Status200OK)]
     [ProducesResponseType(StatusCodes.Status400BadRequest)]
     public async Task<ActionResult> Update([FromBody] CreateClassroomDTO createClassroomDTO, int id){
-        var result = await _classroomService.UpdateClassroomAsync(createClassroomDTO, id);
+        var result = await classroomService.UpdateClassroomAsync(createClassroomDTO, id);
         if (result > 0){
             return Ok(result);
         }
@@ -59,11 +53,11 @@ public class ClassroomsController : BaseController
     [ProducesResponseType(typeof(int), StatusCodes.Status200OK)]
     [ProducesResponseType(StatusCodes.Status400BadRequest)]
     public async Task<ActionResult> Delete(int id){
-        var isChildrenInClassroom = await _classroomService.CheckChildrenInClassroom(id);
+        var isChildrenInClassroom = await classroomService.CheckChildrenInClassroom(id);
         if (isChildrenInClassroom){
             return BadRequest(new ErrorResponse{Message = ["Der er børn i klassen"]});
         }
-        var result = await _classroomService.DeleteClassroomAsync(id);
+        var result = await classroomService.DeleteClassroomAsync(id);
         if (result > 0){
             return Ok(result);
         }
